6f70552588854feff56ccefa780dc3f4186f3c9f
Alex Heflin <alex@gaslight.co>
2014-10-14 15:49:55 -0400

    scrolling of background, bat edge detection

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,26 +4,31 @@
 
 $(document).ready(function() {
 
+  $(document).keydown(function(e) {
+    console.log(e.keyCode);
+    margin_left = $('.character').css('margin-left')
+    margin_left = parseInt(margin_left)
+    left = $('.character').css('left')
+    left = parseInt(left)
+    //left arrow
+    if (e.keyCode == 37) {
+      if (margin_left > 0) {
+        $('.character').animate({'margin-left': '-=50px' }, 500);
+      } else if(margin_left == 0 && left == 10) {
+        $('.background').animate({'margin-left': '0px' }, 500);
+        $('.character').animate({'left': '10px', 'margin-left': '400px' }, 500);
+      }
+    }
+    //right arrow
+    if (e.keyCode == 39) {
+      if (margin_left < 400) {
+        $('.character').animate({'margin-left': '+=50px' }, 500);
+      } else if(margin_left == 400) {
+        $('.background').animate({'margin-left': '-440px' }, 500);
+        $('.character').animate({'left': '10px', 'margin-left': '0px' }, 500);
+      }
+    }
+  });
+
 });
 
-// // left variable isn't getting update on keydown
-// keybindings for left/right
-$(document).bind('keydown', function(e) {
-  console.log(e.keyCode);
-  left = $('.character').position().left
-  //right arrow
-  if (e.keyCode == 39) {
-    if (left >= 9 && left < 400) {
-      $('.character').animate({'margin-left': '-=50' }, 500);
-      console.log('true');
-    } else {
-      console.log('false');
-    }
-  }
-  //left arrow
-  if (e.keyCode == 37) {
-    if (left >= 8) {
-      $('.character').animate({'margin-left': '+=50' }, 500);
-    }
-  }
-});
